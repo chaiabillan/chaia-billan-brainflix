f935d1182b6fef52cbfeb6c458c48b3d638b3963
Chaia Billan <chaia_billan@yahoo.com>
2024-02-20 21:38:40 -0800

    started making the nav bar responsive to different browser sizes

--- src/components/Component/Navbar/Navbar.js
+++ src/components/Component/Navbar/Navbar.js
@@ -11,14 +11,19 @@ function Navbar() {
         </div>
         <div className = "nav-bar__search">
             <input className="nav-bar__search--box"type="text" name="search" placeholder="Search"></input>
-            <span className="nav-bar__search--picture"></span>
+            <span className="nav-bar__picture1"></span>
         </div>
         <div className = "nav-bar__upload">
-                <p className="nav-bar__upload--upload">UPLOAD</p>
-                <img className="nav-bar__upload--icon"src={upload} alt="upload-icon" />            
+            <div className = "nav-bar__upload--button">
+                    <img className="nav-bar__upload--button--icon"src={upload} alt="upload-icon" />            
+                    <p className="nav-bar__upload--button--upload">UPLOAD</p>
+            </div>
+            <div className = "nav-bar__upload--picture">
+                <span className="nav-bar__picture"></span>
+            </div>
         </div>
+        
     </nav>
-
     )
 }
 
