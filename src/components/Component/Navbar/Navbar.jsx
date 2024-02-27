7abe389f5883820d9aa599ac4b7e8483c47730d1
Chaia Billan <chaia_billan@yahoo.com>
2024-02-26 19:21:47 -0800

    start sprint-2

--- src/components/Component/Navbar/Navbar.jsx
+++ src/components/Component/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import './Navbar.scss'
 import logo from '../../../assets/Logo/BrainFlix-logo.svg';
 import upload from '../../../assets/images/Icons/upload.svg';
+import {NavLink} from 'react-router-dom';
 
 
 function Navbar() {
     return (
     <nav className = "nav-bar">
         <div className = "nav-bar__logo nav-bar__left">
-            <img src={logo} alt="logo"/>
+            <NavLink to='/'><img src={logo} alt="logo"/></NavLink>
         </div>
         <div className= "nav-bar__right">
             <div className = "nav-bar__middle">
