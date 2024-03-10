c578330ba5f42b0475ca463d1dd734d7ba0ce117
Chaia Billan <chaia_billan@yahoo.com>
2024-03-10 01:06:32 -0800

    try to add add comment functionality, not quite working yet

--- src/components/Component/VideoPlayerComments/VideoPlayerComments.jsx
+++ src/components/Component/VideoPlayerComments/VideoPlayerComments.jsx
@@ -1,7 +1,14 @@
 import './VideoPlayerComments.scss'
 import comment from '../../../assets/images/Icons/add_comment.svg'
+import React, { useRef } from 'react';
+import axios from 'axios';
+
+
 
 function VideoPlayerComments({currentVideoData}) {
+    const formRef = useRef(null);
+    const api_url = 'http://localhost:8080/videos'
+
     if (!currentVideoData || !currentVideoData.comments) {
         return <p>Loading...</p>;
     }
@@ -13,7 +20,34 @@ function VideoPlayerComments({currentVideoData}) {
     const formatDate = (timestamp) => {
         const date = new Date(timestamp);
         return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-      };
+    };
+
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        const postComment = async () => {
+
+            console.log('formRef.current:', formRef.current);
+
+            const commentData = {
+                'comment': formRef.current.comment.value
+            }
+
+            console.log('commentData:', commentData);
+
+            try {
+                const response = await axios.post(
+                    `http://localhost:8080/videos/${currentVideoData.id}/comments`,
+                    commentData
+                );                
+                formRef.current.reset();
+                return response;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        postComment();
+    };
 
     return (
         <section>
@@ -21,12 +55,12 @@ function VideoPlayerComments({currentVideoData}) {
                 <div className="comment__picture">
                     <span className="comment__picture--icon comment__picture--icon--mohan"></span>
                 </div>
-                <form id = "add-comment-form" className="comment__form">
+                <form ref={formRef} onSubmit={handleClick} id = "add-comment-form" className="comment__form">
                         <label className="comment__form--label comment__form--comment demi">
                             JOIN THE CONVERSATION
                         </label>
                         <div className='comment__form--field--tablet'>
-                            <textarea className="comment__form--field comment__form--field--comment regular" type="text" name="comment" placeholder="Add a new comment"></textarea>
+                            <textarea className="comment__form--field comment__form--field--comment regular" type="text" id="comment" placeholder="Add a new comment"></textarea>
                             
                             <div className="comment__form--button">
                                 <button className="comment__form--button--content demi" type="submit"><img className="comment__form--button--content--icon" src={comment} alt="comment icon"/>COMMENT</button>
